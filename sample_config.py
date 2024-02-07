91c08a2c70a5680ecfe49ace01557627d2494932
rawal01singhbhati <157700273+rawal01singhbhati@users.noreply.github.com>
2024-02-07 15:11:06 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 class Config(object):
-    APP_ID = 000
-    API_HASH = "000"
-    BOT_TOKEN = "000"
+    APP_ID = 9239475
+    API_HASH = "45bca1de511c5133d0d83e0f7f456bee"
+    BOT_TOKEN = "6739131173:AAHDdj0C4xf6vSgkLKpqewxuJrJHOL0E4bs"
     BOT_WORKERS = 40
-    SUDO_USERS = "000,111,222,333"
-    DB_URI = "mongo...."
-    SESSION_NAME = "test"
+    SUDO_USERS = "5796251936"
+    DB_URI = "mongodb+srv://rawalsingh:rawalsingh007@cluster0.pr6ybgb.mongodb.net/?retryWrites=true&w=majority"
+    SESSION_NAME = "BQCM-7MAFI1YWK1RAR8DjPK_OJQtZSu3va29BDOfI7Tb36Ahu7svlf7S77GtEgc5BODbxYMraVOfEYwzeqRb_4qKAqNv7SdxaZhONSPh5cqv3gHYqrTBjC70NpSeDYuw0wgstYjpETYP-nFK1uhMHOqs_DeOcK95kkYUbTlcBY1KrBfXZ_IdcBGRoAr15cs_YfG8fqSg2EKefn9RlKv76vj_FHhLIYIJ0ui2y8mydUhMvV39YD9_tInLUA50JS5IA8vyDcQ8x6byawUboepxDYVkkhedp-g0nxy1lltOMgwVpQMGfg7l-cJ8wQG_Fp0kjOMlt_TmyoGk6IqmRn7XfpltXQEyRQAAAAFZe8kgAA"
     MULTI_USER_MODE = False
